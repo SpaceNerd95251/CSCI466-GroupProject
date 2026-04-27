6da4a43795aefd315934d58d5f2c844cfdd90b35
Adam Brandon <brandonm79bb@gmail.com>
2026-04-27 15:12:31 -0600

    wrote main.php and ajusted header.php

--- header.php
+++ header.php
@@ -9,23 +9,27 @@
 <!DOCTYPE html>
 <html> 
     <head> 
-        <title>CSCI 466 Group Project</title>
+        <!-- shows a different title depending on the page, set in other page files -->
+        <title><?php echo $title; ?></title>
     </head>
     <body> 
 
-        <h1> The Store </h1> // this may be changed depending on what we name the store
+        <h1> The Store </h1> <!-- this may be changed depending on what we name the store -->
 
         <hr> 
+
+        <nav>
             <a href="main.php">Products</a> |
             <a href="cart.php">Cart</a> |
             <a href="orders.php">Orders</a> |
 
-            <?php if (!empty($_SESSION['isAdmin'])): ?>
+            <?php if (!empty($_SESSION['isAdmin'])) { ?>
                 <a href="admin/orders.php">Admin</a> |
                 <a href="admin/fulfillment.php">Fulfillment</a> |
-                <a href="admin.logout.php">Logout</a> 
-            <?php else: ?>
+                <a href="admin/logout.php">Logout</a> 
+            <?php } else { ?>
                 <a href="admin/login.php">Admin Login</a> 
-            <?php endif; ?>
-        </hr>
+            <?php } ?>
+        </nav>
+        <hr>
     </body>
---
+++ main.php
@@ -0,0 +1,34 @@
+<?php 
+    $title = "Products";
+    require 'header.php'; 
+    require 'database/database.php'; 
+
+    // get all products
+    $stmt = $pdo->query("SELECT * FROM products ORDER BY name"); 
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<h2>Products</h2>
+
+<!-- loops through and displays all products with links to more information -->
+<?php foreach ($products as $product) { ?>
+
+     <section>
+        <h3><?php echo $product['name']; ?></h3>
+
+        <img 
+            src="<?php echo $product['image']; ?>" 
+            alt="<?php echo $product['name']; ?>" 
+            width="200"  
+            height="200"
+        >
+
+        <p> Price: <?php echo number_format($product['price'], 2); ?></p>
+        <p> Stock: <?php echo $product['stock']; ?></p>
+        <p> 
+            <a href="products.php?id=<?php echo $product['id']?>">View Details</a>
+        </p>
+    </section>
+
+    <hr>
+<?php } ?>
